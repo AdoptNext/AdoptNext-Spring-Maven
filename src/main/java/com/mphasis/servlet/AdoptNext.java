3d3c9e60bb75c1f4df86119ed6d5d0e0136f5217
AdoptNext <sandeep.bysani@mphasis.com>
2016-05-07 20:15:11 +0530

    AdoptNext Servlet WebApplication, which can deploy war file

---
+++ src/main/java/com/mphasis/servlet/AdoptNext.java
@@ -0,0 +1,45 @@
+package com.mphasis.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdoptNext extends HttpServlet {
+	 
+	  private String message;
+	  private String version;
+	  
+	  public void init() throws ServletException
+	  {
+	      // Do required initialization
+	      message = "ADOPT NEXT";
+	      version = "102";
+	  }
+
+	  public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException
+	  {
+	      // Set response content type
+	      response.setContentType("text/html");
+
+	      // Actual logic goes here.
+	      PrintWriter out = response.getWriter();
+	      out.println("<html><title>AdoptNext</title>");
+	      out.println("<body bgcolor=\"#f0f0f0\">");
+	      out.println("<br><br><br><br><br><br><br><br><br>");
+	      out.println("<h1 style=\"text-align:center\" bgcolor=\"#5F9EA0\"><b>" + message + "</b></h1>");
+	      out.println("<h1 style=\"text-align:center\" bgcolor=\"#5F9EA0\"><b>" + version + "</b></h1>");
+	      
+	      out.println("</body></html>");
+	  }
+	  
+	  public void destroy()
+	  {
+	      // do nothing.
+	  }
+
+}
+ 
